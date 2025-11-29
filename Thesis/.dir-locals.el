a64099a286c69e924744d39d47ec099059e0562d
elvis <elvilio98@gmail.com>
2025-11-29 16:18:21 +0100

    emacs config thesis

---
+++ Thesis/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 80)))
+ (TeX-mode . ((TeX-engine . luatex)
+              (TeX-master . "document.tex")
+              (TeX-command-extra-options . "-shell-escape"))))
